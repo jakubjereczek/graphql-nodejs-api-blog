4d65117ab90b11dc1a2bf20dac07745e122dc8b0
jakubjereczek <99jakubjereczek@gmail.com>
2023-02-17 08:24:57 +0100

    add category into resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,3 +1,4 @@
+import CategoryResolver from './category.resolver';
 import UserResolver from './user.resolver';
 
-export const resolvers = [UserResolver] as const;
+export const resolvers = [UserResolver, CategoryResolver] as const;
